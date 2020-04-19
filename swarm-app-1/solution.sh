32b15609c7eeeb560be76cad99d13741890fa526
Michal Koceba <michal.koceba@gmail.com>
2020-04-19 22:20:55 +0200

    swarm app 1 complete

--- swarm-app-1/solution.sh
+++ swarm-app-1/solution.sh
@@ -4,5 +4,5 @@ docker network create -d overlay backend
 docker service create --network frontend --name vote -p 80:80 --replicas 3 bretfisher/examplevotingapp_vote
 docker service create --network frontend --name redis redis:3.2
 docker service create --name worker --network frontend --network backend bretfisher/examplevotingapp_worker:java
-docker service create --mount type=volume,source=db-data,target=/var/lib/postgresql/data --name db --network postgres:9.4
+docker service create -e POSTGRES_HOST_AUTH_METHOD=trust --mount type=volume,source=db-data,target=/var/lib/postgresql/data --name db --network backend postgres:9.4
 docker service create --name result -p 5001:80 --network backend bretfisher/examplevotingapp_result
